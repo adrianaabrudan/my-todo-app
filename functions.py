82eee6e88a03243a9359491cc73d72af05b0406c
adriana.abrudan <adriana.abrudan@softvision.com>
2022-11-21 10:23:33 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,21 @@
+FILE_PATH = "todos.txt"
+
+
+def get_todos(filepath=FILE_PATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILE_PATH):
+    """ Write a to-do item list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
